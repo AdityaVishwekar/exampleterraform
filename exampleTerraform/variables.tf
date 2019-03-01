f60686a13580d4d660652da5c903f585f040c3c1
Aditya Vishwekar <avishwekar10893@gmail.com>
2019-02-28 20:41:13 -0600

    Update variables.tf

--- exampleTerraform/variables.tf
+++ exampleTerraform/variables.tf
@@ -1,9 +1,15 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-variable "compartment_ocid" {}
+variable "tenancy_ocid" {
+    default = "ocid1.tenancy.oc1..aaaaaaaaqaghoakhcdlsdsej676gkzli4gbeeqw3ge46kgnm224lagmfj4xq"}
+variable "user_ocid" {
+    default = "ocid1.user.oc1..aaaaaaaangsahezapgxbyaj7shxhbgzghxpiumy5bcwadbtln67btb5sampq"}
+variable "fingerprint" {
+    default = "04:cd:31:f8:b4:87:e1:3c:c8:5d:3d:2d:ed:8e:1a:d0"}
+variable "private_key_path" {
+    default = "./oci_api_key.pem"}
+variable "region" {
+    default = "us-ashburn-1"}
+variable "compartment_ocid" {
+    default = "ocid1.compartment.oc1..aaaaaaaajdloco2usq6y24dudqirugj6ej44mpooogjes3penwtye4wp42uq"}
 variable "ssh_public_key" {
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9nFLaLfJZvgcSjhmRnPPpMKVUYiFnxOnoXYDRQXzqmlVSoEToactzTK3l1qvwrg5xsuY9nAFfVPdXtPRoxzuLP+SrDn5mRfoEbDgwtKqdnIeQa0iOUFCHZsJUYJAe1O2VHfegXm3UEMWOKSIJWfElm0iLowEdWgejBeVcSVEQ4lBiYUfhlZbkOp7YFanWWp8Rsq8NufZq6rCgaQKFoZ0n2Jr3f8Me4oAmDnImuFcbyP2McRmoVNyv6kBB1PZ7xXaCk8Kb6ysUF18V1tRkudJBIfSRpVy4nHkm/WyoghnJ2JhTqadiEnC7CPeNLiJpA6AreQOv5a/CRWOI7zqkSpdv"
 }
